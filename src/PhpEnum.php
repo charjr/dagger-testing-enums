8297208a6ba415ea7de88e1569aa383b7fd3e64b
John Charman <john.charman@imhotek.net>
2024-09-05 12:21:30 +0100

    init commit

---
+++ src/PhpEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaggerModule;
+
+enum PhpEnum: string
+{
+   case Version_8_0 = '8.0';
+   case Version_8_3 = '8.3';
+}
---
+++ src/TestingEnums.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaggerModule;
+
+use Dagger\Attribute\Argument;
+use Dagger\Attribute\DaggerFunction;
+use Dagger\Attribute\DaggerObject;
+use Dagger\Container;
+use Dagger\Directory;
+
+use function Dagger\dag;
+
+#[DaggerObject]
+class TestingEnums
+{
+     #[DaggerFunction('Returns a container that echoes whatever string argument is provided')]
+     public function containerEcho(string $stringArg): Container
+     {
+         return dag()
+             ->container()
+             ->from('alpine:latest')
+             ->withExec(['echo', $stringArg]);
+     }
+
+    #[DaggerFunction('Returns lines that match a pattern in the files of the provided Directory')]
+     public function grepDir(
+         #[Argument('The directory to search')]
+         Directory $directoryArg,
+         #[Argument('The pattern to search for')]
+         string $pattern
+    ): string {
+         return dag()
+             ->container()
+             ->from('alpine:latest')
+             ->withMountedDirectory('/mnt', $directoryArg)
+             ->withWorkdir('/mnt')
+             ->withExec(["grep", '-R', $pattern, '.'])
+             ->stdout();
+     }
+}
